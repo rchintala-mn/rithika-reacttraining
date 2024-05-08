92899c142c60e14895fded01da5df1e7e7455950
rchintala-mn <110657541+rchintala-mn@users.noreply.github.com>
2024-05-08 14:59:36 +0530

    Update AxiosCakedetails.js
    updated addtocart

--- AxiosCakedetails.js
+++ AxiosCakedetails.js
@@ -24,23 +24,52 @@ export default function AxiosCakedetails() {
     );
   }, []);
   console.log("cake details = ", cakedetails);
+    function addToCart() {
+    axios({
+      url: "http://apibyauw.eu-4.evennode.com/api" + "/addcaketocart",
+      method: "post",
+      data: {
+        cakeid: cakedetails.cakeid,
+        name: cakedetails.name,
+        price: cakedetails.price,
+        image: cakedetails.image,
+        weight: cakedetails.weight,
+      },
+      headers: {
+        Authorization: localStorage.token,
+      },
+    }).then(
+      (response) => {
+        console.log("add to cart response:", response);
+        setCakedetails(response.data.data); //after response ,pass it to setCakedetails to view in Checkout
+        alert("Cake is added to Cart");
+        navigate("/detailCard/" + cakeid);
+      },
+      (error) => {
+        console.log("Error from API: ", error);
+        alert("Error Cake is not added");
+      }
+    );
+  }
   return (
     // Style="text-align:center;"
-    <div>
-      <img
-        preview={true}
-        src={cakedetails.image}
-        width={500}
-        style={{ float: "left", marginRight: "10px" }}
-      />
-      <li>Name : {cakedetails.name}</li>
-      <br />
-      <li>Price : {cakedetails.price}</li>
-      <br />
+    <div style={{ display: "flex", alignItems: "center" }}>
+      <div style={{ marginRight: "20px" }}>
+        <Image preview={true} src={cakedetails.image} width={500} />
+      </div>
 
-      <Button style={{ float: "center", marginRight: "10px" }}>
-        Add to Cart
-      </Button>
+      <div>
+        <ul>
+          <li>Name : {cakedetails.name}</li>
+          <li>Price : {cakedetails.price}</li>
+          <li>Rating : {cakedetails.ratings}</li>
+          <li>Description : {cakedetails.description}</li>
+
+          <Button type="primary" onClick={addToCart}>
+            Add to Cart
+          </Button>
+        </ul>
+      </div>
     </div>
   );
 }
